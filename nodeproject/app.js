42c880540e7d81560a7468df2f45fdfb03fc9400
Jayson Grace <jayson.e.grace@gmail.com>
2017-01-29 21:35:22 +0000

    Add updated app.js and gulpfile

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,23 @@
-var bla = 'hi';
-console.log(bla);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT;
+
+app.use(express.static('public'));
+app.use(express.static('bower_components'));
+ 
+app.set('views', '/src/views');
+app.set('view engine', 'ejs');
+ 
+app.get('/', function(req, res){
+        // res.send('HAI!');
+    res.render('index', { list: ['first', 'second', 'third']});
+});
+
+app.get('/routing', function(req, res){
+    res.send('Routes 4 lyfe!');
+});
+
+app.listen(port, function(err){
+    console.log('The server is running on port: ' + port); 
+})
