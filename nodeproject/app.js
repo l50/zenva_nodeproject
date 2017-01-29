19f9f6a07635647e6e1acdd57217c974d2c497b3
Jayson Grace <jayson.e.grace@gmail.com>
2017-01-29 21:58:11 +0000

    Add bower.json file and updated app.js

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -6,7 +6,7 @@ var port = process.env.PORT;
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
  
-app.set('views', '/src/views');
+app.set('views', './src/views');
 app.set('view engine', 'ejs');
  
 app.get('/', function(req, res){
