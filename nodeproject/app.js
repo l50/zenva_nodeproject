4ca477b40d8e91607acbec0ebed48e39e59ffb72
Jayson Grace <jayson.e.grace@gmail.com>
2017-01-30 06:48:08 +0000

    Add router code

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT;
+var eventRouter = express.Router();
 
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
@@ -9,9 +10,30 @@ app.use(express.static('bower_components'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
  
+eventRouter.route('/')
+    .get(function(req, res){
+        res.send('Hello events');
+    })
+    
+eventRouter.route('/event')
+    .get(function(req, res){
+        res.send('Hello single event');
+    })
+    
+app.use('/Events', eventRouter)
+    
+ 
 app.get('/', function(req, res){
         // res.send('HAI!');
-    res.render('index', { list: ['first', 'second', 'third']});
+    res.render('index', { 
+        list: ['first', 'second', 'third'],
+        nav: [{Link: 'Services', Text: 'Services'},
+        {Link: 'Portfolio', Text: 'Portfolio'},
+        {Link: 'About', Text: 'About'},
+        {Link: 'Contact', Text: 'Contact'},
+        {Link: 'Events', Text: 'Events'}
+        ]
+    });
 });
 
 app.get('/routing', function(req, res){
